c049d742ec931804be6a22cbaaf29a33ef44d73c
Gschillereff <55813029+Gschillereff@users.noreply.github.com>
2020-03-22 16:05:08 -0700

    Added Scoring plane, and score display

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,54 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    public static float speed = 500;
     public Rigidbody rb;
+    public bool boosting = false;
+    public static float boostSpeed = 3000;
+    public float boostTime = 2.0f;
+    public float currentBoostTime;
+    public float currentBoostDelayTime;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        currentBoostTime = 0f;
+        currentBoostDelayTime = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movePlayer();
     }
 
-    void FixedUpdate()
+    void movePlayer()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(movement*speed*Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space) && !boosting && Time.time > currentBoostDelayTime)
+        {
+            currentBoostTime = Time.time + boostTime;
+            boosting = true;
+        }
+
+        if((Time.time > currentBoostTime) && boosting)
+        {
+            boosting = false;
+            currentBoostDelayTime = Time.time + currentBoostDelayTime;
+        }
+
+        if(boosting)
+        {
+            speed = boostSpeed;
+        }
+        else
+        {
+            speed = 500;
+        }
 
     }
 }
---
+++ Assets/Scripts/scoreDetector.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreDetector : MonoBehaviour
+{
+    public int score;
+    public int displayScore;
+    public Text scoreUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        displayScore = 0;
+        scoreUI.text = "Score: " + displayScore;
+ 
+        StartCoroutine(ScoreUpdater());
+   
+    }
+
+    private IEnumerator ScoreUpdater()
+    {
+        while(true)
+        {
+            if(displayScore < score)
+            {
+                displayScore++;
+                scoreUI.text = "Score: " + displayScore.ToString();
+            }
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag =="Ball")
+        {
+            score++;
+           
+        }
+    }
+}
